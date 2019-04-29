9afddbe1543edcb26c0c4f33b9bd96baf080430c
insanefury <justdarsan@gmail.com>
2019-04-29 00:55:10 -0300

    Added Controllers

---
+++ Assets/Resources/Scripts/JetController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetController : MonoBehaviour
+{
+    public float EnginePower = 50f;
+    float RollEffect = 1f;
+    float PitchEffect = 1f;
+    float YawEffect = 0.2f;
+    float mouseX = 0f;
+    float mouseY = 0f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * EnginePower * Time.deltaTime);
+        }
+        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), Input.GetAxis("Horizontal")) * Time.deltaTime * EnginePower);
+    }
+
+}
---
+++ Assets/Resources/Scripts/Limiter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limiter : MonoBehaviour
+{
+    bool playerIsOutsideTheLimit = false;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsOutsideTheLimit = true;
+            Debug.Log("Player are outside the limit");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsOutsideTheLimit = false;
+            Debug.Log("Player are inside the limit");
+        }
+    }
+}
